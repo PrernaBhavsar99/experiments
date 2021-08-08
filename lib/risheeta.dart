452a16bc9a624d39018e06314b647ae3e4cb12ea
risheetajain <risheetajain309@gmail.com>
2021-08-08 14:28:17 +0530

    new file created with different name

---
+++ lib/risheeta.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RisheetaJain extends StatefulWidget {
+  const RisheetaJain({Key? key}) : super(key: key);
+
+  @override
+  _RisheetaJainState createState() => _RisheetaJainState();
+}
+
+class _RisheetaJainState extends State<RisheetaJain> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Hii guys "),
+      ),
+    );
+  }
+}
