e09d9f55e6d0fa661b815524d171be9bcd4f2440
PrernaBhavsar99 <prerna.bhavsar2018@gmail.com>
2021-08-08 14:30:46 +0530

    done

---
+++ lib/screen/new.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Exoscreen extends StatelessWidget {
+  const Exoscreen({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+       appBar: AppBar(
+         title: Text("helo"),
+       ),
+       
+      
+    );
+  }
+}
